d38d0328d892e5542468831a0ded4530754f31c9
joejohnson0 <joelau94@gmail.com>
2018-11-06 00:45:01 -0500

    fix running scripts

--- scripts/train_pos_en.sh
+++ scripts/train_pos_en.sh
@@ -1,15 +1,15 @@
 python codes/main.py \
     --train-gemb false \
-    --config-file ../models/pos-en-config.pkl \
-    --train-data-file ../data/pos_en/train.txt \
-    --dev-data-file ../data/pos_en/dev.txt \
-    --test-data-file ../data/pos_en/test.txt \
+    --config-file models/pos-en-config.pkl \
+    --train-data-file data/pos_en/train.txt \
+    --dev-data-file data/pos_en/dev.txt \
+    --test-data-file data/pos_en/test.txt \
     --task tagging \
     --keep-prob 0.9 \
-    --dictfile ../data/pos_en/dicts.pkl \
+    --dictfile data/pos_en/dicts.pkl \
     --embed-dim 300 \
     --hidden-dims 256 256 \
-    --ckpt ../models/pos-en-model \
+    --ckpt models/pos-en-model \
     --max-ckpts 20 \
     --batch-size 64 \
     --max-steps 1000000 \
